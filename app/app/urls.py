efef928ed9f8c7362d5bfa08072953f0d9c7e278
Rob Killick <robkillick@Robs-MacBook-Pro-2.local>
2024-07-02 17:53:14 +0100

    modify settings and docker setup for static and media files

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,6 +20,8 @@ from drf_spectacular.views import (
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,4 +34,9 @@ urlpatterns = [
     path('api/auth/', include('user.urls')),
     path('api/location/', include('location.urls')),
 ]
-    
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
